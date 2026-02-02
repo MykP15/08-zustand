e67f27b4cb9d80bc272b3187b375dca44d2903b2
Mykhailo Pyroh <mykhailopk@gmail.com>
2026-02-02 15:35:12 +0100

    fix NoteForm props

--- app/notes/action/create/CreateNote.client.tsx
+++ app/notes/action/create/CreateNote.client.tsx
@@ -9,7 +9,7 @@ function CreateNoteClient() {
              <main className={css.main}>
   <div className={css.container}>
     <h1 className={css.title}>Create note</h1>
-	   <NoteForm onClose={() => {}}></NoteForm>
+	   <NoteForm></NoteForm>
   </div>
 </main>
          </>
