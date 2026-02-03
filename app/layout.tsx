e53b464743167b787e00a1e48ace0885af3c8ca6
Mykhailo Pyroh <mykhailopk@gmail.com>
2026-02-03 11:12:17 +0100

    Remove duplicate roboto.variable in body class

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${roboto.variable} ${roboto.variable}`}>
+      <body className={`${roboto.variable}`}>
         <TanStackProvider>
         <Header />
           <div>{children}
